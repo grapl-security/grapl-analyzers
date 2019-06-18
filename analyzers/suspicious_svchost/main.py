161505c9342a31c7c572355ad33ab1d887c38a0e
Insanitybit <insanitybit@gmail.com>
2019-06-17 20:03:08 -0700

    Add suspicious svchost analyzer

---
+++ analyzers/suspicious_svchost/main.py
@@ -0,0 +1,34 @@
+
+from typing import Any
+
+from grapl_analyzerlib.execution import ExecutionHit
+from pydgraph import DgraphClient
+from grapl_analyzerlib.entities import ProcessQuery, SubgraphView, FileQuery
+
+
+def analyzer(client: DgraphClient, graph: SubgraphView, sender: Any):
+
+    valid_parents = [
+        "smss.exe",
+        "ngentask.exe",
+        "userinit.exe",
+    ]
+
+    for process in graph.process_iter():
+        p = (
+            ProcessQuery()
+            .with_process_name(eq=valid_parents)
+            .with_children(
+                ProcessQuery().with_process_name(eq="svchost.exe")
+            )
+            .query_first(client, contains_node_key=process.node_key)
+        )
+
+        if p:
+            sender.send(
+                ExecutionHit(
+                    analyzer_name="Suspicious svchost",
+                    node_view=p,
+                    risk_score=10,
+                )
+            )
