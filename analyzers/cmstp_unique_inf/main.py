75fea1f4b10413fd203c36d9d18e44b655fe092e
Insanitybit <insanitybit@gmail.com>
2019-06-27 19:42:50 -0700

    Skeleton for caching. Commented due to cdk bug.

--- analyzers/cmstp_unique_inf/main.py
+++ analyzers/cmstp_unique_inf/main.py
@@ -1,9 +1,40 @@
+import os
 
 from typing import Any, Optional
+
+import redis
 
 from grapl_analyzerlib.execution import ExecutionHit, ExecutionComplete
 from pydgraph import DgraphClient
 from grapl_analyzerlib.entities import ProcessQuery, SubgraphView, FileQuery, ProcessView, NodeView
+
+
+# COUNTCACHE_ADDR = os.environ['COUNTCACHE_ADDR']
+# COUNTCACHE_PORT = os.environ['COUNTCACHE_PORT']
+#
+# r = redis.Redis(host=COUNTCACHE_ADDR, port=COUNTCACHE_PORT, db=0)
+
+
+def count_path(dg_client, path, max=4):
+    # cached_count = r.get(path)
+    # if cached_count and cached_count >= max:
+    #     return cached_count
+
+    count = (
+        ProcessQuery().with_process_name(ends_with="CMSTP.exe")
+            .with_read_files(
+            FileQuery().with_file_path(eq=path)
+        )
+        .get_count(dg_client, max=4)
+    )
+
+    # if count >= max:
+    #     if not cached_count:
+    #         r.set(path, count)
+    #     elif count >= cached_count:
+    #         r.set(path, count)
+
+    return count
 
 
 def analyzer(client: DgraphClient, node: NodeView, sender: Any):
@@ -24,14 +55,10 @@ def analyzer(client: DgraphClient, node: NodeView, sender: Any):
 
     for read_file in p.read_files:
         # See if at least 4 combinations exist of CMSTP with this file name.
-        f = (
-            FileQuery()
-            .with_file_path(eq=read_file)
-            .get_count(client, max=4)
-        )
+        count = count_path(client, read_file.get_file_path())
 
         # If fewer than 4 returned, this is rare enough to track
-        if f < 4:
+        if count < 4:
             sender.send(
                 ExecutionHit(
                     analyzer_name="CMSTP with Unique INF",
