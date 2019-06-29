acc7d5b231e3219b23ef7951809a0e182dad54d4
Insanitybit <insanitybit@gmail.com>
2019-06-29 11:04:38 -0700

    Disable some analyzers

--- analyzers/execution_from_unpacked_file/main.py
+++ analyzers/execution_from_unpacked_file/main.py
@@ -8,29 +8,31 @@ from grapl_analyzerlib.entities import ProcessQuery, SubgraphView, FileQuery, No
 
 def analyzer(client: DgraphClient, node: NodeView, sender: Any):
 
-    unpackers = ["7zip.exe", "winrar.exe", "zip.exe"]
-
-    process = node.as_process_view()
-    if not process:
-        return
-
-    p = (
-        ProcessQuery()
-        .with_bin_file(
-            FileQuery().with_creator(
-                ProcessQuery()
-                .with_process_name(eq=unpackers)
-            )
-        )
-        .query_first(client, contains_node_key=process.node_key)
-    )
-
-    if p:
-        sender.send(
-            ExecutionHit(
-                analyzer_name="Process Executing From Unpacked File",
-                node_view=p,
-                risk_score=15,
-            )
-        )
+    # TODO: Reenable
+    return
+    # unpackers = ["7zip.exe", "winrar.exe", "zip.exe"]
+    #
+    # process = node.as_process_view()
+    # if not process:
+    #     return
+    #
+    # p = (
+    #     ProcessQuery()
+    #     .with_bin_file(
+    #         FileQuery().with_creator(
+    #             ProcessQuery()
+    #             .with_process_name(eq=unpackers)
+    #         )
+    #     )
+    #     .query_first(client, contains_node_key=process.node_key)
+    # )
+    #
+    # if p:
+    #     sender.send(
+    #         ExecutionHit(
+    #             analyzer_name="Process Executing From Unpacked File",
+    #             node_view=p,
+    #             risk_score=15,
+    #         )
+    #     )
 
--- analyzers/unique_parent_child/main.py
+++ analyzers/unique_parent_child/main.py
@@ -9,38 +9,40 @@ from grapl_analyzerlib.entities import ProcessQuery, SubgraphView, NodeView, Pro
 
 def analyzer(client: DgraphClient, node: NodeView, sender: Any):
 
-    counter = ParentChildCounter(client)
-
-    process = node.as_process_view()
-    if not process:
-        return
-
-    p = (
-        ProcessQuery()
-        .with_process_name()
-        .with_parent(
-            ProcessQuery()
-            .with_process_name()
-        )
-        .query_first(client, contains_node_key=process.node_key)
-    )  # type: Optional[ProcessView]
-
-    if not p:
-        return
-
-    parent = p.get_parent()
-
-    count = counter.get_count_for(
-        parent_process_name=p.get_process_name(),
-        child_process_name=parent.get_process_name(),
-        excluding=process.node_key
-    )
-
-    if count < Seen.Many:
-        sender.send(
-            ExecutionHit(
-                analyzer_name="Rare Parent Child Process",
-                node_view=p,
-                risk_score=70,
-            )
-        )
+    # TODO: Reenable this analyzer
+    return
+    # counter = ParentChildCounter(client)
+    #
+    # process = node.as_process_view()
+    # if not process:
+    #     return
+    #
+    # p = (
+    #     ProcessQuery()
+    #     .with_process_name()
+    #     .with_parent(
+    #         ProcessQuery()
+    #         .with_process_name()
+    #     )
+    #     .query_first(client, contains_node_key=process.node_key)
+    # )  # type: Optional[ProcessView]
+    #
+    # if not p:
+    #     return
+    #
+    # parent = p.get_parent()
+    #
+    # count = counter.get_count_for(
+    #     parent_process_name=p.get_process_name(),
+    #     child_process_name=parent.get_process_name(),
+    #     excluding=process.node_key
+    # )
+    #
+    # if count < Seen.Many:
+    #     sender.send(
+    #         ExecutionHit(
+    #             analyzer_name="Rare Parent Child Process",
+    #             node_view=p,
+    #             risk_score=70,
+    #         )
+    #     )
