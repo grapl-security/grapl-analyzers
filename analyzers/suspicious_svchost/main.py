850e925a2c68420d586fa088075e2e702e58c0db
Colin <insanitybit@gmail.com>
2019-09-15 17:34:10 -0700

    Update main.py

--- analyzers/suspicious_svchost/main.py
+++ analyzers/suspicious_svchost/main.py
@@ -23,7 +23,7 @@ def analyzer(client: DgraphClient, node: NodeView, sender: Any):
     
     p = (
         ProcessQuery()
-        .with_process_name(eq=inalid_parents)
+        .with_process_name(eq=invalid_parents)
         .with_children(
             ProcessQuery().with_process_name(eq="svchost.exe")
         )
