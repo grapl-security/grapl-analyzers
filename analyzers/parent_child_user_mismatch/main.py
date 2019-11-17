4414b4a996d9e5cae8d3d95c053b340ac16de4dc
Insanitybit <insanitybit@gmail.com>
2019-11-16 19:04:06 -0800

    Integrate new plugins

--- analyzers/parent_child_user_mismatch/main.py
+++ analyzers/parent_child_user_mismatch/main.py
@@ -1,47 +1,59 @@
-# from typing import Any
-#
-# from grapl_analyzerlib.analyzer import Analyzer, OneOrMany
-# from grapl_analyzerlib.prelude import ProcessQuery, ProcessView
-# from grapl_analyzerlib.execution import ExecutionHit
-#
-#
-# class ParentChildUserMismatch(Analyzer):
-#
-#     def get_queries(self) -> OneOrMany[ProcessQuery]:
-#         # TODO: Whitelist based on parent, like sudo
-#         return (
-#             ProcessQuery()
-#             .with_user_id()
-#             .with_parent(
-#                 ProcessQuery()
-#                 .with_user_id()
-#             ),
-#             ProcessQuery()
-#             .with_auid()
-#             .with_parent(
-#                 ProcessQuery()
-#                 .with_auid()
-#             ),
-#          )
-#
-#     def on_response(self, child: ProcessView, output: Any):
-#         parent = child.get_parent()
-#
-#         uid_mismatch = False
-#         auid_mismatch = False
-#
-#         if parent.get_user_id() is not None and child.get_user_id() is not None:
-#             uid_mismatch = parent.get_user_id() != child.get_user_id()
-#
-#         if parent.get_auid() is not None and child.get_auid() is not None:
-#             auid_mismatch = parent.get_auid() != child.get_auid()
-#
-#         if uid_mismatch or auid_mismatch:
-#
-#             output.send(
-#                 ExecutionHit(
-#                     analyzer_name="Parent Child User Mismatch",
-#                     node_view=child,
-#                     risk_score=25,
-#                 )
-#             )
+from typing import *
+
+from grapl_analyzerlib.analyzer import Analyzer, OneOrMany
+from grapl_analyzerlib.prelude import ProcessQuery, ProcessView
+from grapl_analyzerlib.execution import ExecutionHit
+from grapl_os_user_analyzer_plugin.assumed_user_id_node import AssumedUserIdView, AssumedUserIdQuery
+from grapl_os_user_analyzer_plugin.user_id_node import UserIdQuery
+
+
+def with_assumed_user_id(process: ProcessQuery) -> ProcessQuery:
+    # The ssh_process must have an associated user id
+    (
+        AssumedUserIdQuery()
+            .with_assuming_process(process)
+            .with_user_id()
+    )
+    return process
+
+
+def get_user_id(process: ProcessView) -> Optional[int]:
+    user_assumption = (
+        AssumedUserIdQuery()
+        .with_assuming_process(ProcessQuery().with_node_key(process.node_key))
+        .with_user_id()
+        .query_first(process.dgraph_client)
+    )  # type: Optional[AssumedUserIdView]
+    if user_assumption:
+        return user_assumption.get_used_id()
+
+    return None
+
+
+class ParentChildUserMismatch(Analyzer):
+
+    def get_queries(self) -> OneOrMany[ProcessQuery]:
+
+        parent = with_assumed_user_id(ProcessQuery())
+        child = with_assumed_user_id(ProcessQuery())
+        child.with_parent(parent)
+
+        return (
+            child
+         )
+
+    def on_response(self, child: ProcessView, output: Any):
+        parent = child.get_parent()
+
+        child_user_id = get_user_id(child)
+        parent_user_id = get_user_id(parent)
+
+        if child_user_id != parent_user_id:
+
+            output.send(
+                ExecutionHit(
+                    analyzer_name="Parent Child User Mismatch",
+                    node_view=child,
+                    risk_score=25,
+                )
+            )
--- analyzers/ssh_agent_access_lineage_uid_auid_mismatch/main.py
+++ analyzers/ssh_agent_access_lineage_uid_auid_mismatch/main.py
@@ -1,232 +1,141 @@
-# from copy import deepcopy
-# from typing import Union, Optional, Any, List, Tuple, Callable, Type
-#
-# from grapl_analyzerlib.entities import DynamicNodeQuery, ProcessView, PV
-# from grapl_analyzerlib.querying import Viewable, V, StrCmp, IntCmp
-# from pydgraph import DgraphClient
-#
-# import os
-# from typing import Set
-#
-# import redis
-# from grapl_analyzerlib.analyzer import Analyzer, OneOrMany
-# from grapl_analyzerlib.entities import ProcessQuery
-# from grapl_analyzerlib.execution import ExecutionHit
-#
-# COUNTCACHE_ADDR = os.environ['COUNTCACHE_ADDR']
-# COUNTCACHE_PORT = os.environ['COUNTCACHE_PORT']
-#
-# r = redis.Redis(host=COUNTCACHE_ADDR, port=COUNTCACHE_PORT, db=0, decode_responses=True)
-#
-# from copy import deepcopy
-# from typing import Union, Optional, Any, List, Tuple, Callable, Type
-#
-# from grapl_analyzerlib.entities import DynamicNodeQuery, ProcessView, PV
-# from grapl_analyzerlib.querying import Viewable, V, StrCmp, IntCmp
-# from pydgraph import DgraphClient
-#
-#
-# class InterProcessCommunicationQuery(DynamicNodeQuery):
-#     def __init__(self) -> None:
-#         super(InterProcessCommunicationQuery, self).__init__("InterProcessCommunication", InterProcessCommunicationView)
-#
-#     def with_key(self, eq=StrCmp, contains=StrCmp, ends_with=StrCmp) -> 'InterProcessCommunicationQuery':
-#         self.with_property_str_filter("key", eq=eq, contains=contains, ends_with=ends_with)
-#         return self
-#
-#     def with_src_pid(self, eq=IntCmp, gt=IntCmp, lt=IntCmp) -> 'InterProcessCommunicationQuery':
-#         self.with_property_int_filter("src_pid", eq=eq, gt=gt, lt=lt)
-#         return self
-#
-#     def with_dst_pid(self, eq=IntCmp, gt=IntCmp, lt=IntCmp) -> 'InterProcessCommunicationQuery':
-#         self.with_property_int_filter("dst_pid", eq=eq, gt=gt, lt=lt)
-#         return self
-#
-#     def with_ipc_type(self, eq=StrCmp, contains=StrCmp, ends_with=StrCmp) -> 'InterProcessCommunicationQuery':
-#         self.with_property_str_filter("ipc_type", eq=eq, contains=contains, ends_with=ends_with)
-#         return self
-#
-#     def with_ipc_creator(self, created_ipc: 'PQ') -> 'InterProcessCommunicationQuery':
-#         created_ipc = deepcopy(created_ipc)
-#         self.with_edge_filter("created_ipc", created_ipc)
-#         created_ipc.with_reverse_edge_filter("~created_ipc", self)
-#         return self
-#
-#     def with_ipc_recipient(self, received_ipc: 'PQ') -> 'InterProcessCommunicationQuery':
-#         received_ipc = deepcopy(received_ipc)
-#         self.with_edge_filter("received_ipc", received_ipc)
-#         received_ipc.with_reverse_edge_filter("~received_ipc", self)
-#         return self
-#
-#
-# class InterProcessCommunicationView(Viewable):
-#
-#     def __init__(
-#             self,
-#             dgraph_client: DgraphClient,
-#             node_key: str,
-#             uid: str,
-#             key: str = None,
-#             src_pid: int = None,
-#             dst_pid: int = None,
-#             ipc_type: str = None,
-#             created_ipc: Optional["PV"] = None,
-#             received_ipc: Optional["PV"] = None,
-#             **kwargs,
-#     ):
-#         super(InterProcessCommunicationView, self).__init__(dgraph_client, node_key, uid)
-#         self.key = key
-#         self.src_pid = src_pid
-#         self.dst_pid = dst_pid
-#         self.ipc_type = ipc_type
-#
-#         self.created_ipc = created_ipc
-#         self.received_ipc = received_ipc
-#
-#     @staticmethod
-#     def get_property_types() -> List[Tuple[str, Callable[[Any], Union[str, int]]]]:
-#         return [
-#             ("key", str),
-#             ("ipc_type", str),
-#             ("src_pid", int),
-#             ("dst_pid", int),
-#         ]
-#
-#     @staticmethod
-#     def get_edge_types() -> List[Tuple[str, Union[List[Type[V]], Type[V]]]]:
-#         return [
-#             ("created_ipc", ProcessView),
-#             ("received_ipc", ProcessView),
-#         ]
-#
-#     def get_property_tuples(self) -> List[Tuple[str, Any]]:
-#         prop_tuples = [
-#             ("node_key", self.node_key),
-#             ("key", self.key),
-#             ("src_pid", self.src_pid),
-#             ("dst_pid", self.dst_pid),
-#             ("ipc_type", self.ipc_type),
-#         ]
-#
-#         return [pt for pt in prop_tuples if pt[1]]
-#
-#
-#     def get_edge_tuples(self) -> List[Tuple[str, Union[List[Type[V]], Type[V]]]]:
-#         edge_tuples = [
-#             ("created_ipc", self.created_ipc),
-#             ("received_ipc", self.received_ipc),
-#         ]
-#
-#         return [et for et in edge_tuples if et[1]]
-#
-#     @staticmethod
-#     def get_edges() -> List[Tuple[str, Union[List[Type[V]], Type[V]]]]:
-#         return [
-#             ("created_ipc", ProcessView),
-#             ("received_ipc", ProcessView),
-#         ]
-#
-#     def get_neighbors(self):
-#         return [
-#             n for n in (self.created_ipc, self.received_ipc) if n
-#         ]
-#
-#     def get_key(self) -> Optional[str]:
-#         self.key = self.get_property('key', str)
-#         return self.key
-#
-#     def get_src_pid(self) -> Optional[int]:
-#         self.src_pid = self.get_property('src_pid', int)
-#         return self.src_pid
-#
-#     def get_dst_pid(self) -> Optional[int]:
-#         self.dst_pid = self.get_property('dst_pid', int)
-#         return self.dst_pid
-#
-#     def get_ipc_type(self) -> Optional[str]:
-#         self.ipc_type = self.get_property('ipc_type', str)
-#         return self.ipc_type
-#
-#     def get_ipc_creator(self) -> Optional['PV']:
-#         self.created_ipc = self.get_edge("created_ipc", ProcessView)
-#         return self.created_ipc
-#
-#     def get_ipc_recipient(self) -> Optional['PV']:
-#         self.received_ipc = self.get_edge("received_ipc", ProcessView)
-#         return self.received_ipc
-#
-#
-# def get_uid_auid_lineage(
-#         cur_root: Optional[ProcessView],
-#         user_ids: Set[int] = None,
-#         auids: Set[int] = None,
-# ) -> Tuple[Set[int], Set[int]]:
-#
-#     if not user_ids:
-#         user_ids = set()
-#
-#     if not auids:
-#         auids = set()
-#
-#     if not cur_root:
-#         return user_ids, auids
-#
-#     cur_user_id = cur_root.get_user_id()
-#     cur_auid = cur_root.get_auid()
-#
-#     if cur_user_id is not None:
-#         user_ids.add(cur_user_id)
-#     if cur_auid is not None:
-#         auids.add(cur_auid)
-#
-#     return get_uid_auid_lineage(cur_root.get_parent(), user_ids, auids)
-#
-#
-# class SshAgentAccessLineageAuidOrUidMismatch(Analyzer):
-#     # Look for IPC access where the target is ssh-agent
-#     def get_queries(self) -> OneOrMany[InterProcessCommunicationQuery]:
-#         return (
-#             # Query to check for mismatch of uid
-#             InterProcessCommunicationQuery()
-#             .with_ipc_creator(
-#                 ProcessQuery().with_user_id()
-#             )
-#             .with_ipc_recipient(
-#                 ProcessQuery()
-#                     .with_user_id()
-#                     .with_process_name(eq='ssh-agent')
-#                     .with_process_name(eq='sshd')
-#             ),
-#             # Query to check for mismatch of auid
-#             InterProcessCommunicationQuery()
-#             .with_ipc_creator(
-#                 ProcessQuery().with_auid()
-#             )
-#             .with_ipc_recipient(
-#                 ProcessQuery()
-#                     .with_auid()
-#                     .with_process_name(eq='ssh-agent')
-#                     .with_process_name(eq='sshd')
-#             )
-#         )
-#
-#     def on_response(self, response: InterProcessCommunicationView, output: Any):
-#         print(f'Received suspicious IPC view: {response.node_key}')
-#
-#         src_uids, src_auids = get_uid_auid_lineage(response.get_ipc_creator())
-#         tgt_uids, tgt_auids = get_uid_auid_lineage(response.get_ipc_recipient())
-#
-#         user_mismatch = (src_uids.issuperset(tgt_uids) or src_uids.issubset(tgt_uids))
-#         auid_mismatch = (src_auids.issuperset(tgt_auids) or src_auids.issubset(tgt_auids))
-#
-#         if user_mismatch or auid_mismatch:
-#             output.send(
-#                 ExecutionHit(
-#                     analyzer_name="Ssh Agent Access: UID or AUID mismatch in lineage",
-#                     node_view=response,
-#                     risk_score=100,
-#                 )
-#             )
-#
-#
-#
+import os
+from typing import *
+
+import redis
+from grapl_analyzerlib.analyzer import Analyzer
+from grapl_analyzerlib.execution import ExecutionHit
+from grapl_analyzerlib.nodes.process_node import ProcessView
+from grapl_os_user_analyzer_plugin.assumed_auid_node import AssumedAuidQuery
+
+from analyzers.ssh_agent_ipc.main import IpcQuery, IpcView
+
+COUNTCACHE_ADDR = os.environ['COUNTCACHE_ADDR']
+COUNTCACHE_PORT = os.environ['COUNTCACHE_PORT']
+
+r = redis.Redis(host=COUNTCACHE_ADDR, port=int(COUNTCACHE_PORT), db=0, decode_responses=True)
+
+from grapl_analyzerlib.nodes.types import OneOrMany
+from grapl_analyzerlib.prelude import ProcessQuery
+
+from grapl_os_user_analyzer_plugin.assumed_user_id_node import AssumedUserIdQuery, AssumedUserIdView
+
+
+def with_assumed_user_id(process: ProcessQuery) -> ProcessQuery:
+    # The ssh_process must have an associated user id
+    (
+        AssumedUserIdQuery()
+        .with_assuming_process(process)
+        .with_user_id()
+    )
+    return process
+
+
+def with_assumed_auid(process: ProcessQuery):
+    # The ssh_process must have an associated auid
+    (
+        AssumedAuidQuery()
+        .with_assuming_process(process)
+        .with_auid()
+    )
+    return process
+
+def get_user_id(process: ProcessView) -> Optional[int]:
+    user_assumption = (
+        AssumedUserIdQuery()
+        .with_assuming_process(ProcessQuery().with_node_key(process.node_key))
+        .with_user_id()
+        .query_first(process.dgraph_client)
+    )  # type: Optional[AssumedUserIdView]
+    if user_assumption:
+        return user_assumption.get_used_id()
+
+    return None
+
+def get_auid(process: ProcessView) -> Optional[int]:
+    auid_assumption = (
+        AssumedAuidQuery()
+        .with_assuming_process(ProcessQuery().with_node_key(process.node_key))
+        .with_auid()
+        .query_first(process.dgraph_client)
+    )  # type: Optional[AssumedUserIdView]
+    if auid_assumption:
+        return auid_assumption.get_auid()
+
+    return None
+
+from typing import Set
+
+def get_uid_auid_lineage(
+        cur_root: Optional[ProcessView],
+        user_ids: Set[int] = None,
+        auids: Set[int] = None,
+) -> Tuple[Set[int], Set[int]]:
+    if not user_ids:
+        user_ids = set()
+
+    if not auids:
+        auids = set()
+
+    if not cur_root:
+        return user_ids, auids
+
+    cur_user_id = get_user_id(cur_root)
+    cur_auid = get_auid(cur_root)
+
+    if cur_user_id is not None:
+        user_ids.add(cur_user_id)
+    if cur_auid is not None:
+        auids.add(cur_auid)
+
+    return get_uid_auid_lineage(cur_root.get_parent(), user_ids, auids)
+
+
+class SshAgentAccessLineageAuidOrUidMismatch(Analyzer):
+    # Look for IPC access where the target is ssh-agent
+    def get_queries(self) -> OneOrMany[IpcQuery]:
+
+        ssh_process_with_user = with_assumed_user_id(
+            ProcessQuery()
+            .with_process_name(eq='ssh-agent')
+            .with_process_name(eq='sshd')
+        )
+        ipc_creator_with_user = with_assumed_user_id(ProcessQuery())
+
+        ssh_process_with_auid = with_assumed_auid(
+            ProcessQuery()
+            .with_process_name(eq='ssh-agent')
+            .with_process_name(eq='sshd')
+        )
+        ipc_creator_with_auid = with_assumed_auid(ProcessQuery())
+
+        return (
+            # Query to check for mismatch of uid
+            IpcQuery()
+            .with_ipc_creator(ssh_process_with_user)
+            .with_ipc_recipient(ipc_creator_with_user),
+            # Query to check for mismatch of auid
+            IpcQuery()
+            .with_ipc_creator(ssh_process_with_auid)
+            .with_ipc_recipient(ipc_creator_with_auid),
+        )
+
+    def on_response(self, response: IpcView, output: Any):
+        print(f'Received suspicious IPC view: {response.node_key}')
+
+        src_uids, src_auids = get_uid_auid_lineage(response.get_ipc_creator())
+        tgt_uids, tgt_auids = get_uid_auid_lineage(response.get_ipc_recipient())
+
+        user_mismatch = (src_uids.issuperset(tgt_uids) or src_uids.issubset(tgt_uids))
+        auid_mismatch = (src_auids.issuperset(tgt_auids) or src_auids.issubset(tgt_auids))
+
+        if user_mismatch or auid_mismatch:
+            output.send(
+                ExecutionHit(
+                    analyzer_name="Ssh Agent Access: UID or AUID mismatch in lineage",
+                    node_view=response,
+                    risk_score=100,
+                )
+            )
+
+
+
